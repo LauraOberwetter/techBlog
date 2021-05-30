4df343b0358732e2f0c253947fe1cbffea840660
Laura Oberwetter <leoberwetter@gmail.com>
2021-05-30 14:03:05 -0500

    dashboardRoutes formatting fixed

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,25 +1,27 @@
-const router = require('express').Router();
-const { Post } = require('../models');
-const withAuth = require('../utils/auth');
+const router = require("express").Router();
+const { Post } = require("../models");
+const withAuth = require("../utils/auth");
 
 //if current user_id matches blog posts, GET posts
-router.get('/', async (req, res) => {
-    try {
-      // Get all posts and JOIN with user data
-      const postData = await Post.findAll({
-        where: {
-            user_id: req.session.user_id,
-          },
-      });
+router.get("/", async (req, res) => {
+  try {
+    // Get all posts and JOIN with user data
+    const postData = await Post.findAll({
+      where: {
+        user_id: req.session.user_id,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
 // make new post after clicking btn
 router.post("/", withAuth, (req, res) => {
-    BlogPosts.create({
-      user_id: req.session.user_id,
-      title: req.body.newTitle,
-      content: req.body.newPostBody,
-    })
-      .then(() => res.redirect("/dashboard"))
-      .catch((err) => res.status(500).json(err));
-  });
-
-  });
+  BlogPosts.create({
+    user_id: req.session.user_id,
+    title: req.body.newTitle,
+    content: req.body.newPostBody,
+  })
+    .then(() => res.redirect("/dashboard"))
+    .catch((err) => res.status(500).json(err));
+});
