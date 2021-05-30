3013ff802ddb5c15162cfabdd63210eb4922da40
Laura Oberwetter <leoberwetter@gmail.com>
2021-05-30 13:30:09 -0500

    added makepost.js logic and makepost.handlebars form

--- public/js/makepost.js
+++ public/js/makepost.js
@@ -2,6 +2,27 @@
 const makePostHandler = async (event) => {
     event.preventDefault();
 
-// Collect values from the login form
-const makePostTitle = document.querySelector('#email-login').value.trim();
-const makePostBody = document.querySelector('#password-login').value.trim();
+// Collect values from the make-post form
+const makePostTitle = document.querySelector('#make-post-title').value.trim();
+const makePostBody = document.querySelector('#make-post-body').value.trim();
+
+if (makePostTitle && makePostBody) {
+    // Send a POST request to the API endpoint
+    const response = await fetch('/api/post', {
+      method: 'POST',
+      body: JSON.stringify({ makePostTitle, makePostBody }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      // If successful, post to dashboard
+      document.location.replace('/dashboard');
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+document
+.querySelector('.make-post-form')
+.addEventListener('submit', makePostFormHandler);
